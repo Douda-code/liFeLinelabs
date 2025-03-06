4a812c0af57b803e74de5e96799c3eaaa75983cf
Douda-code <derradjadel0@gmail.com>
2025-03-06 15:23:18 +0100

    Update BottomNavBar.vue

--- components/BottomNavBar.vue
+++ components/BottomNavBar.vue
@@ -6,12 +6,12 @@ const darkModeStore = useDarkModeStore()
 </script>
 
 <template>
-  <nav class="fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-800 shadow-lg z-50">
+  <nav class="bg-white dark:bg-indigo-700 dark:bg-opacity-25 shadow-lg">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex justify-center h-16 items-center">
         <div class="flex space-x-8">
           <RouterLink
-            to="/about"
+            to="/"
             class="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white transition-colors duration-200"
           >
             About Us
@@ -38,7 +38,4 @@ const darkModeStore = useDarkModeStore()
       </div>
     </div>
   </nav>
-
-  <!-- Spacer div to prevent content from being hidden behind the fixed navbar -->
-  <div class="h-16"></div>
 </template>
